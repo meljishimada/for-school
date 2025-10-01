0fee582843fc269630536e2482ba3b1c36090cae
melji <meljishimada@gmail.com>
2025-10-01 14:06:44 -0400

    First version

---
+++ vecteur.cpp
@@ -0,0 +1,5 @@
+//
+// Created by melji on 2025-09-30.
+//
+
+#include "vecteur.h"
---
+++ vecteur.h
@@ -0,0 +1,21 @@
+//
+// Created by melji on 2025-09-30.
+//
+
+#ifndef VENDREDI_VECTEUR_H
+#define VENDREDI_VECTEUR_H
+#include<cstddef>
+
+
+class vecteur {
+private:
+    int* adresse;
+    size_t cardinal; // Entier non-signer
+
+public:
+
+}
+;
+
+
+#endif //VENDREDI_VECTEUR_H
